c2436bc2b2c22853e8f65045fc44f0756b1e9d51
侯建宇 <houjianyu@houjianyudeMacBook-Pro.local>
2021-01-25 20:05:29 +0800

    完成了后台主页

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+// 发送ajax请求 获取用户信息
+function getUserInfo () {
+    axios.get("/my/userinfo").then((res)=> {
+        console.log(res);
+        if (res.data.status !==0) {
+            return  layer.msg('获取用户信息失败')
+        }
+        avatarAndName(res.data)
+    })
+}
+getUserInfo()
+function avatarAndName (res) {
+    // console.log('res打印结果为',res);
+    let name = res.data.nickname||res.data.username 
+    $('#weicome').text('欢迎'+name)
+    $('#come').text(name)
+    if (res.data.user_pic) {
+        $('.layui-nav-img').attr('src',res.data.user_pic).show()
+        $('.text-avatar').hide()
+    }else {
+        $('.layui-nav-img').hide()
+        let first = name[0].toUpperCase()
+        $(".text-avatar").text(first).show()
+    }
+ $('#logoutBtn').click(function() {
+    layer.confirm('确定退出', {icon: 3, title:'提示'}, function(index){
+        //do something
+         localStorage.removeItem('token')
+         location.href = '/home/login.html'
+        layer.close(index);
+      });
+ })
+ 
+}
